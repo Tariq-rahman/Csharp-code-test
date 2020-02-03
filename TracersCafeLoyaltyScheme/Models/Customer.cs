b9fe896d0386cf683c4867beec2f7bf5f53ce7de
Tariq <Tariqurahman@gmail.com>
2020-02-03 12:06:33 +0000

    Update telephone validation

--- TracersCafeLoyaltyScheme/Models/Customer.cs
+++ TracersCafeLoyaltyScheme/Models/Customer.cs
@@ -16,8 +16,10 @@ namespace TracersCafeLoyaltyScheme.Models
         public Title Title { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the Firstname")]
+        [StringLength(maximumLength: 100, ErrorMessage = "Firstname is too long")]
         public String Firstname { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the Lastname")]
+        [StringLength(maximumLength: 100, ErrorMessage = "Last name is too long")]
         public String Lastname { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the first line of address")]
         public String Adress1 { get; set; }
@@ -25,9 +27,10 @@ namespace TracersCafeLoyaltyScheme.Models
         public String Adress3 { get; set; }
         public String Adress4 { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the Postcode")]
+        [StringLength(maximumLength: 7, ErrorMessage = "Enter a valid postcode")]
         public String Postcode { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a Telephone number")]
-        [StringLength(maximumLength: 11, MinimumLength = 11, ErrorMessage = "Please enter valid UK number")]
+        [StringLength(maximumLength: 11, MinimumLength = 11, ErrorMessage = "Please enter valid UK number")]        
         public String Telephone { get; set; }
         public int Age { get; set; }
         //public DateTime CreatedDate { get; set; }
